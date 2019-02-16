ccf47aba506eaabbcb7fc07e2232339b741f963b
Xannden <Xannden@gmail.com>
2019-02-16 13:45:17 -0600

    Add files missing from last commit

---
+++ CompendiumMapCreator/Data/ToolTipAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CompendiumMapCreator.Data
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public class ToolTipAttribute : Attribute
+	{
+		public string ToolTip { get; }
+
+		public ToolTipAttribute(string tooltip)
+		{
+			this.ToolTip = tooltip;
+		}
+	}
+}
