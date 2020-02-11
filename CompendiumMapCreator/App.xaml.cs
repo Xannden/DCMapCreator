54d83501d4d6270f7172ee9c0f827b4e39fbf086
Xannden <Xannden@gmail.com>
2020-02-11 12:59:46 -0600

    Bump version number

--- CompendiumMapCreator/App.xaml.cs
+++ CompendiumMapCreator/App.xaml.cs
@@ -230,7 +230,7 @@ namespace CompendiumMapCreator
 
 			Temp temp = (Temp)serializer.ReadObject(response.GetResponseStream());
 
-			return temp.tag_name != "V1.8";
+			return temp.tag_name != "V1.8.1";
 		}
 
 		[DataContract]
--- CompendiumMapCreator/Properties/AssemblyInfo.cs
+++ CompendiumMapCreator/Properties/AssemblyInfo.cs
@@ -29,12 +29,12 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
+	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+									 //(used if a resource is not found in the page,
+									 // or application resource dictionaries)
+	ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+											  //(used if a resource is not found in the page,
+											  // app, or any theme specific resource dictionaries)
 )]
 
 // Version information for an assembly consists of the following four values:
@@ -47,4 +47,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.*")]
+[assembly: AssemblyVersion("1.8.1.*")]
